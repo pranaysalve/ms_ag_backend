f5abbc11551bdbd9ac5a24b4377be589b3820ff2
Pranay Salve <64769068+pranaysalve@users.noreply.github.com>
2023-09-08 03:47:02 +0530

    Update routes.js

--- APIGateway/routes/routes.js
+++ APIGateway/routes/routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const Controller = require("../controller/handler.controller");
 
+
 router.post("/login", Controller.login).post("/signup", Controller.signup);
 
 module.exports = router;
